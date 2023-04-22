1046462ac5fc7eba589e233784f80d8f953431d0
Ofir Somech <ofirsomech4@gmail.com>
2023-04-22 18:41:49 +0300

    move login fnction to utils file

--- src/modules/scrapper/services/scrapper.service.ts
+++ src/modules/scrapper/services/scrapper.service.ts
@@ -5,6 +5,7 @@ import { LinkService } from '../../Link/services/link.service';
 import { ScriptService } from '../../script/services/script.service';
 import { ScreenshotService } from '../../screenshot/services/screenshot.service';
 import { StylesheetService } from '../../stylesheet/services/stylesheet.service';
+import { addToAllScansData } from '../utils/utils';
 
 @Injectable()
 export class ScrapperService {
@@ -55,39 +56,18 @@ export class ScrapperService {
     const allScripts = await this.scriptService.find();
 
     for (const item of allScreenshots) {
-      this.addToAllScansData(allScansData, item, 'screenshots');
+      addToAllScansData(allScansData, item, 'screenshots');
     }
     for (const item of allLinks) {
-      this.addToAllScansData(allScansData, item, 'links');
+      addToAllScansData(allScansData, item, 'links');
     }
     for (const item of allStylesheets) {
-      this.addToAllScansData(allScansData, item, 'stylesheets');
+      addToAllScansData(allScansData, item, 'stylesheets');
     }
     for (const item of allScripts) {
-      this.addToAllScansData(allScansData, item, 'scripts');
+      addToAllScansData(allScansData, item, 'scripts');
     }
 
     return allScansData;
-  }
-
-  addToAllScansData(allScansData, item, type) {
-    const website = item.url;
-    const date = new Date(item.createdAt).toLocaleDateString('en-GB', {
-      timeZone: 'UTC',
-    });
-
-    if (!allScansData[website]) {
-      allScansData[website] = {};
-    }
-
-    if (!allScansData[website][date]) {
-      allScansData[website][date] = {};
-    }
-
-    if (!allScansData[website][date][type]) {
-      allScansData[website][date][type] = [];
-    }
-
-    allScansData[website][date][type].push(item);
   }
 }
